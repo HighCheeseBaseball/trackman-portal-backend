8feb213a66b34763ce9bcece3cb74a6c88bdd10f
HighCheeseBaseball <135353514+HighCheeseBaseball@users.noreply.github.com>
2025-07-30 11:12:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ app.use(express.json());
 
 // Add CORS support
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', '*');
   
   // Handle preflight requests
   if (req.method === 'OPTIONS') {
